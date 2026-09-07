2831cf24bd78bf744eb14470de4afe5b10e79309
Sophie Student <sophie.student16@gmail.com>
2026-09-07 17:38:06 +0000

    first commit of helloWorldUpdated.cpp

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Sophie!" << std::endl;
+    std::cout << "Programming is SO much FUN!\n";
+    std::cout << "I cannot wait to learn more:)" << std::endl;
+}
